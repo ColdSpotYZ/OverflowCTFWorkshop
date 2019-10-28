a2ce42e1fc2d7917e3328193539db79b287e1cf3
Jason <jason@Jasons-MacBook-Air-3.local>
2019-10-29 00:54:27 +0800

    Fix some bus

--- Misc/Crack Me/solution.py
+++ Misc/Crack Me/solution.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 from pwn import *
 
-host, port = "www.coldspot.wtf", 8081
+host, port = "www.coldspot.wtf", 8080
 
 s = connect(host, port)
 
 s.recvline()
 for i in range(1000):
 	s.sendline('A')
-	temp = s.recvline()
+	s.recvline()
 	if "HNF" in temp:
 		print(temp)
